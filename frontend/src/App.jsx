2602caed1af116fdf7f86dca30f619f947d0ee9f
Your New Name <abhiramskumar75@gmail.com>
2025-08-11 10:44:43 +0530

    added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
+// App.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// Pages
+import { Routes, Route } from "react-router-dom";
 
 import Aboutus from "./pages/AboutUs";
 import LandingPage from "./pages/LandingPage";
@@ -11,35 +10,24 @@ import Contact from "./pages/Contact";
 import DigitalMarketing from "./pages/DigitalMarketing";
 import WebDevelopment from "./pages/WebDevelopment";
 import WeddingEvents from "./pages/WeddingEvents";
-
-// Components (can be nested or used in pages)
 import Ads from "./components/Ads";
 import Photoshoots from "./components/Photoshoots";
 import Videoshoots from "./components/Videoshoots";
 
-
-function App() {
+export default function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Pages */}
-
-        <Route path="/" element={<LandingPage/>} />
-         <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<Aboutus />} />
-        <Route path="/branding-media" element={<BrandingMedia />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/digital-marketing" element={<DigitalMarketing />} />
-        <Route path="/web-development" element={<WebDevelopment />} />
-        <Route path="/wedding" element={<WeddingEvents />} />
-
-        {/* Component routes (optional if you want direct routes to these) */}
-        <Route path="/ads" element={<Ads />} />
-        <Route path="/photoshoots" element={<Photoshoots />} />
-        <Route path="/videoshoots" element={<Videoshoots />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<Aboutus />} />
+      <Route path="/branding-media" element={<BrandingMedia />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/digital-marketing" element={<DigitalMarketing />} />
+      <Route path="/web-development" element={<WebDevelopment />} />
+      <Route path="/wedding" element={<WeddingEvents />} />
+      <Route path="/ads" element={<Ads />} />
+      <Route path="/photoshoots" element={<Photoshoots />} />
+      <Route path="/videoshoots" element={<Videoshoots />} />
+    </Routes>
   );
 }
-
-export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import { BrowserRouter } from 'react-router-dom';
+// main.jsx
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
-
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-   <BrowserRouter> <App /></BrowserRouter>
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
